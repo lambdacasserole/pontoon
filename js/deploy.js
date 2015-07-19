999490b41dce8b3c4b64b192863cf0a098a0191c
Saul Johnson <saul.johnson@traveljunction.com>
2015-07-19 15:06:47 +0100

    Add return message block

--- js/deploy.js
+++ js/deploy.js
@@ -2,8 +2,7 @@ function deploy(hash) {
     waitingDialog.show('Deploying site');
     $.get('go.php?project=' + hash, function(data) {
         var result = JSON.parse(data);
-        $('.col-msg').html('<div class="alert alert-' + result.status + '">' + result.message + '</div>');
+        $('.col-msg').html('<div class="alert alert-' + result.status + '">' + result.message + '</div><pre>' + result.result + '</pre>');
         waitingDialog.hide();
-        alert(result.result);
     });
 }
