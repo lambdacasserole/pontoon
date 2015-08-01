ff5dab090ffc58a300ee2b66fa14e6ace488729d
Saul Johnson <saul.johnson@traveljunction.com>
2015-08-01 16:13:47 +0100

    Document CSS, JS and configuration class

--- js/deploy.js
+++ js/deploy.js
@@ -1,8 +1,20 @@
+/*
+ * Project: Pontoon
+ * Author: Saul Johnson
+ * Notes: Client-side script for the deployments page.
+ */
+
+/**
+ * Uses an AJAX call to deploy a website.
+ *
+ * @param hash  the hash (unique ID) of the project to deploy
+ */
 function deploy(hash) {
-    waitingDialog.show('Deploying site');
+    waitingDialog.show('Deploying site', {});
     $.get('go.php?project=' + hash, function(data) {
         var result = JSON.parse(data);
-        $('.col-msg').html('<div class="alert alert-' + result.status + '">' + result.message + '</div><pre>' + result.result + '</pre>');
+        $('.col-msg').html('<div class="alert alert-' + result.status + '">' + result.message
+            + '</div><pre>' + result.result + '</pre>');
         waitingDialog.hide();
     });
 }
--- libs/Configuration.php
+++ libs/Configuration.php
@@ -11,41 +11,76 @@ class Configuration
     private static $instance;
     private $config;
 
+    /**
+     * Private constructor for a handle on the application configuration file.
+     *
+     * @param string $path  the path to read the configuration file from
+     */
     private function __construct($path)
     {
         $this->config = Spyc::YAMLLoad($path);
     }
 
+    /**
+     * Gets the root path that contains website directories.
+     *
+     * @return string   the root directory path
+     */
     public function getRootPath()
     {
         return $this->config['root_path'];
     }
 
+    /**
+     * Gets the name of the deploy configuration file to look for.
+     *
+     * @return string
+     */
     public function getDeployConfigName()
     {
         return $this->config['deploy_config_name'];
     }
 
+    /**
+     * Gets the admin email address required for login to the deployments page.
+     *
+     * @return string
+     */
     public function getAdminEmail()
     {
         return $this->config['admin_email'];
     }
 
+    /**
+     * Gets the password hash required for login to the deployments page.
+     *
+     * @return string
+     */
     public function getAdminPasswordHash()
     {
         return $this->config['admin_password_hash'];
     }
 
+    /**
+     * Gets the secret key used by the application for symmetric encryption purposes.
+     *
+     * @return string
+     */
     public function getSecretKey()
     {
         return $this->config['secret_key'];
     }
 
+    /**
+     * Gets the single instance of this class.
+     *
+     * @return Configuration
+     */
     public static function get()
     {
         if (self::$instance == null)
         {
-            self::$instance = new Configuration('config.yml');
+            self::$instance = new Configuration('config.yml'); // Load configuration.
         }
         return self::$instance;
     }
